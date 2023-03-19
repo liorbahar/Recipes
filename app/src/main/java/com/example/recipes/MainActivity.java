88d3587c280b8129a2efe503d01bc7076f4dd75c
afekl <afekev55@gmail.com>
2023-03-19 18:40:00 +0200

    change style of menu and viewRecipe

--- app/src/main/java/com/example/recipes/MainActivity.java
+++ app/src/main/java/com/example/recipes/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.recipes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         if (!ModelClient.instance().users.isUserLogIn()) {
             navController.navigate(R.id.action_recipesListPageFragment_to_loginFragment);
         }
+
+        this.changeActionBarBackgroundColor();
     }
 
     int fragmentMenuId = 0;
@@ -68,4 +73,10 @@ public class MainActivity extends AppCompatActivity {
     public void showSupportActionBar() {
         getSupportActionBar().show();
     }
+    public void changeActionBarBackgroundColor() {
+        ActionBar actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#68868E"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+    }
+
 }
